67e9d7e99c8d8c00d7f649895dca284611bef882
ALi Javed <muhammadaliadil999@gmail.com>
2022-10-10 17:31:08 +0500

    'ertefg'

--- sendingemail/settings.py
+++ sendingemail/settings.py
@@ -16,11 +16,11 @@ from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = 'django-insecure-y81bk1g9y9y4bwj4w)6j#yz=_+f8$(l-kx7oxra^rolbswigsj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
